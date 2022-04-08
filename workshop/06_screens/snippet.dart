c4f34d609260b4b211c611a4e178c937e42ad300
Anna Leushchenko <anna.domashych@gmail.com>
2022-04-08 18:58:28 +0300

    Update Step 6

--- workshop/06_screens/snippet.dart
+++ workshop/06_screens/snippet.dart
@@ -1,41 +1,39 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, curly_braces_in_flow_control_structures
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ExampleApp());
+  runApp(ExampleApp());
 }
 
 class ExampleApp extends StatelessWidget {
-  const ExampleApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.green,
-            // TODO 1: Provide value for background field
+          seedColor: Colors.green,
+          // TODO 1: Provide value for background field
         ),
         // TODO 2: Provide value for appBarTheme field
       ),
-      home: const ExamplePage(),
+      home: ExamplePage(),
     );
   }
 }
 
 class ExamplePage extends StatelessWidget {
-  const ExamplePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Consistent design with Flutter Theme'),
+        title: Text('Consistent design with Flutter Theme'),
         actions: [
-          IconButton(icon: const Icon(Icons.account_circle), onPressed: (){},),
-          IconButton(icon: const Icon(Icons.adb), onPressed: (){},),
+          IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
+          IconButton(icon: Icon(Icons.email), onPressed: () {}),
         ],
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Center(
           child: ExampleWidget(),
@@ -46,15 +44,8 @@ class ExamplePage extends StatelessWidget {
 }
 
 class ExampleWidget extends StatelessWidget {
-  const ExampleWidget({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: const [
-        Text('Styling screens'),
-      ],
-    );
+    return Text('Styling screens');
   }
 }
